53c51b9adad033c19cd85178dec3477b84a6f45c
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-07 14:58:42 +0700

    fungsi display

--- binarysearch_0194.cpp
+++ binarysearch_0194.cpp
@@ -48,3 +48,17 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n================ Elemen Array Setelah Dijurutkan (Asc) ================\n";
+    cout << "=======================================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
